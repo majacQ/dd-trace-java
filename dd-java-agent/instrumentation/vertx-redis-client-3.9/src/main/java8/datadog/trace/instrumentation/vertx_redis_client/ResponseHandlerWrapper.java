d0eb46241445eb7ccd7d5b6bef9e23d5924ca491
Björn Antonsson <bjorn.antonsson@datadoghq.com>
2021-04-21 14:14:07 +0200

    Change Vert.x 3.9 RedisClient spans to not enclose the handler

--- dd-java-agent/instrumentation/vertx-redis-client-3.9/src/main/java8/datadog/trace/instrumentation/vertx_redis_client/ResponseHandlerWrapper.java
+++ dd-java-agent/instrumentation/vertx-redis-client-3.9/src/main/java8/datadog/trace/instrumentation/vertx_redis_client/ResponseHandlerWrapper.java
@@ -1,9 +1,7 @@
 package datadog.trace.instrumentation.vertx_redis_client;
 
-import static datadog.trace.bootstrap.instrumentation.api.AgentTracer.activateSpan;
-
-import datadog.trace.bootstrap.instrumentation.api.AgentScope;
 import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
+import datadog.trace.context.TraceScope;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Handler;
 import io.vertx.redis.client.Response;
@@ -11,26 +9,32 @@ import io.vertx.redis.client.Response;
 public class ResponseHandlerWrapper implements Handler<AsyncResult<Response>> {
   private final Handler<AsyncResult<Response>> handler;
   private final AgentSpan span;
+  private final TraceScope.Continuation continuation;
 
   public ResponseHandlerWrapper(
-      final Handler<AsyncResult<Response>> handler, final AgentSpan span) {
+      final Handler<AsyncResult<Response>> handler,
+      final AgentSpan span,
+      final TraceScope.Continuation continuation) {
     this.handler = handler;
     this.span = span;
+    this.continuation = continuation;
   }
 
   @Override
   public void handle(final AsyncResult<Response> event) {
-    AgentScope scope = null;
+    TraceScope scope = null;
     try {
       if (null != span) {
-        scope = activateSpan(span);
+        span.finish();
+      }
+      if (null != continuation) {
+        scope = continuation.activate();
       }
       handler.handle(event);
     } finally {
       if (null != scope) {
         scope.close();
       }
-      span.finish();
     }
   }
 }
