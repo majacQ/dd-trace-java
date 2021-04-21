c70e0eddc33a7cef73b415ce84185f9796efad29
Björn Antonsson <bjorn.antonsson@datadoghq.com>
2021-04-21 15:37:35 +0200

    Change Vert.x 3.9 MySQLClient spans to not enclose the handler

--- dd-java-agent/instrumentation/vertx-mysql-client-3.9/src/main/java8/datadog/trace/instrumentation/vertx_sql_client/QueryResultHandlerWrapper.java
+++ dd-java-agent/instrumentation/vertx-mysql-client-3.9/src/main/java8/datadog/trace/instrumentation/vertx_sql_client/QueryResultHandlerWrapper.java
@@ -1,9 +1,7 @@
 package datadog.trace.instrumentation.vertx_sql_client;
 
-import static datadog.trace.bootstrap.instrumentation.api.AgentTracer.activateSpan;
-
-import datadog.trace.bootstrap.instrumentation.api.AgentScope;
 import datadog.trace.bootstrap.instrumentation.api.AgentSpan;
+import datadog.trace.context.TraceScope;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Handler;
 import io.vertx.sqlclient.SqlResult;
@@ -12,25 +10,32 @@ public class QueryResultHandlerWrapper<T, R extends SqlResult<T>>
     implements Handler<AsyncResult<R>> {
   private final Handler<AsyncResult<R>> handler;
   private final AgentSpan span;
+  private final TraceScope.Continuation continuation;
 
-  public QueryResultHandlerWrapper(final Handler<AsyncResult<R>> handler, final AgentSpan span) {
+  public QueryResultHandlerWrapper(
+      final Handler<AsyncResult<R>> handler,
+      final AgentSpan span,
+      final TraceScope.Continuation continuation) {
     this.handler = handler;
     this.span = span;
+    this.continuation = continuation;
   }
 
   @Override
   public void handle(final AsyncResult<R> event) {
-    AgentScope scope = null;
+    TraceScope scope = null;
     try {
       if (null != span) {
-        scope = activateSpan(span);
+        span.finish();
+      }
+      if (null != continuation) {
+        scope = continuation.activate();
       }
       handler.handle(event);
     } finally {
       if (null != scope) {
         scope.close();
       }
-      span.finish();
     }
   }
 }
